32680a40586eb5d0abf4ff1aa1763ec2400155bc
Enrico1077 <enrico.snijders@hs-osnabrueck.de>
2024-02-15 11:32:01 +0100

    Changes for Testing

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,8 @@ def create_app(test_config=None):
     @app.after_request              #Verhindert CORS-Fehler
     def add_headers(response):
         """ Adding some global properties to all response headers """
-        response.headers["Access-Control-Allow-Origin"] = "https://projektstudium.xn--fr-den-bachelor-zvb.eu"
+        #response.headers["Access-Control-Allow-Origin"] = "https://projektstudium.xn--fr-den-bachelor-zvb.eu"
+        response.headers["Access-Control-Allow-Origin"] = "*"
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
         response.headers["Access-Control-Allow-Credentials"] = "true"
